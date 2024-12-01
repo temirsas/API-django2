042b36842974efafaa4077c69b33e47c4a62951d
Temir <temirsas@gmail.com>
2024-12-01 17:49:31 +0600

    add permissions

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,32 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class BasePermission(object):
+    """
+    A base class from which all permission classes should inherit.
+    """
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
