f98b3a2379fcb74698d1d4935bb1f4f04bbd6b6c
Temir <temirsas@gmail.com>
2024-12-01 18:54:06 +0600

    add schema and documentation

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,21 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model 
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser 
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+from rest_framework import generics
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser] 
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
@@ -13,6 +26,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)  
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
 
 class BasePermission(object):
     """
